d6e9c02af3d9fccee2aec788cf1fec8a1622ccb5
vlong638 <vlong638@163.com>
2023-07-01 17:33:37 +0800

    项目分化

---
+++ VL.Gaming25D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Gaming25D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
