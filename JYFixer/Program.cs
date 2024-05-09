93f10b83f6ef574b3a34fa14478d289788ced682
vlong638 <vlong638@163.com>
2024-05-09 14:19:21 +0800

    1.PixelConverter

--- JYFixer/Program.cs
+++ JYFixer/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VL.Gaming.Framework.Tools;
 
 namespace JYFixer
 {
@@ -10,9 +6,17 @@ namespace JYFixer
     {
         static void Main(string[] args)
         {
+            // 输入图片路径
+            string inputImagePath = "D:\\input_image.jpg";
 
+            // 输出图片路径
+            string outputImagePath = "D:\\output_pixel_art.png";
 
+            // 指定像素画大小
+            int pixelSize = 32;
 
+            // 调用转换方法
+            PixelConverter.ConvertToPixelArt(inputImagePath, outputImagePath, pixelSize);
         }
     }
 }
---
+++ VL.Gaming.Common/Framework/Tools/PixelConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace VL.Gaming.Framework.Tools
+{
+    public class PixelConverter
+    {
+        public static void ConvertToPixelArt(string inputImagePath, string outputImagePath, int pixelSize)
+        {
+            // 打开输入图片
+            Bitmap inputImage = new Bitmap(inputImagePath);
+
+            // 创建一个新的Bitmap对象，将输入图片调整为指定大小
+            Bitmap outputImage = new Bitmap(inputImage, new Size(pixelSize, pixelSize));
+
+            // 保存输出图片
+            outputImage.Save(outputImagePath);
+
+            // 释放资源
+            inputImage.Dispose();
+            outputImage.Dispose();
+        }
+    }
+}
