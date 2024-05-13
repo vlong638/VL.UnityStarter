99f1d9d2fcc5bdd2d4b635adead5a33bd440972a
vlong638 <vlong638@163.com>
2024-05-14 07:07:20 +0800

    DialogueBox 1.说话者切换 2.发言状态切换:变大, 3.逐字吐字效果

--- JYFixer/Program.cs
+++ JYFixer/Program.cs
@@ -1,4 +1,6 @@
-﻿using VL.Gaming.Framework.Tools;
+﻿using System.Collections.Generic;
+using System.IO;
+using VL.Gaming.Framework.Tools;
 
 namespace JYFixer
 {
@@ -6,6 +8,9 @@ namespace JYFixer
     {
         static void Main(string[] args)
         {
+            MockData_DialogueData();
+
+
             // 输入图片路径
             string inputImagePath = "D:\\input_image.jpg";
 
@@ -17,6 +22,43 @@ namespace JYFixer
 
             // 调用转换方法
             PixelConverter.ConvertToPixelArt(inputImagePath, outputImagePath, pixelSize);
+        }
+
+        private static void MockData_DialogueData()
+        {
+            //造数据
+            List<Dialogue> testData = new List<Dialogue>();
+            testData.Add(new Dialogue(PortraitLocation.Left, "Sprites\\DialogueTest_猎人", "猎人", "终于找到你了"));
+            testData.Add(new Dialogue(PortraitLocation.Right, "Sprites\\DialogueTest_大灰狼", "大灰狼", "我只是个普普通通的老婆婆"));
+            testData.Add(new Dialogue(PortraitLocation.Left, "Sprites\\DialogueTest_猎人", "猎人", "别装了,你这只愚蠢的大灰狼"));
+            testData.Add(new Dialogue(PortraitLocation.Right, "Sprites\\DialogueTest_大灰狼", "大灰狼", "什么?什么大灰狼"));
+            testData.Add(new Dialogue(PortraitLocation.Left, "Sprites\\DialogueTest_猎人", "猎人", "你的狼尾巴都露出来了"));
+            testData.Add(new Dialogue(PortraitLocation.Left, "Sprites\\DialogueTest_猎人", "猎人", "受死吧!可恶的大灰狼"));
+            File.WriteAllText(@"D:\\游戏开发\\测试数据\\DialogueTest.json", Newtonsoft.Json.JsonConvert.SerializeObject(testData));
+        }
+    }
+
+
+    public enum PortraitLocation
+    {
+        None,
+        Left,
+        Right,
+    }
+
+    public class Dialogue
+    {
+        public PortraitLocation portraitLocation;
+        public string portraitSource;
+        public string title;
+        public string content;
+
+        public Dialogue(PortraitLocation portraitLocation, string portraitSource, string title, string content)
+        {
+            this.portraitLocation = portraitLocation;
+            this.portraitSource = portraitSource;
+            this.title = title;
+            this.content = content;
         }
     }
 }
--- VL.Gaming.Common/Unity/Gaming/DisplayManage/DialogueManager.cs
+++ VL.Gaming.Common/Unity/Gaming/DisplayManage/DialogueManager.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,31 +13,26 @@ namespace VL.Gaming.Unity.Gaming.DisplayManage
         [SerializeField]
         public Text textContent;
         [SerializeField]
-        public string dialogueSource; // 外部指定的Json文件路径
-        private List<Dialogue> dialogues; // 存储对话数据的列表
-        private int currentDialogueIndex = 0;
-        [SerializeField]
-        public GameObject DialogueBox;
+        public GameObject dialogueBox;
+        List<Dialogue> dialogues;
+        int currentDialogueIndex = 0;
 
+        float timer = 0f;
+        public Image leftPortrait;
+        string lastLeftSource;
+        public Image rightPortrait;
+        string lastRightSource;
         void Awake()
         {
-            dialogueSource = "D:\\test.json";
-            //造数据
-            //List<Dialogue> testData = new List<Dialogue>();
-            //testData.Add(new Dialogue("猎人", "终于找到你了"));
-            //testData.Add(new Dialogue("大灰狼", "我只是个普普通通的老婆婆"));
-            //testData.Add(new Dialogue("猎人", "别装了,你这只愚蠢的大灰狼"));
-            //testData.Add(new Dialogue("大灰狼", "什么?什么大灰狼"));
-            //testData.Add(new Dialogue("猎人", "你的狼尾巴都露出来了"));
-            //testData.Add(new Dialogue("猎人", "受死吧!可恶的大灰狼"));
-            //File.WriteAllText(dialogueSource, Newtonsoft.Json.JsonConvert.SerializeObject(testData));
+            leftPortrait = GameObject.Find("Image_LeftPortrait").GetComponent<Image>();
+            rightPortrait = GameObject.Find("Image_RightPortrait").GetComponent<Image>();
         }
 
         void OnEnable()
         {
             Debug.LogWarning("DialogueManager OnEnable");
             LoadDialogueData();
-            UpdateUI();
+            GoNext();
         }
 
         void Start()
@@ -44,59 +40,174 @@ namespace VL.Gaming.Unity.Gaming.DisplayManage
         }
 
         //生效间隔
-        private float lastClickTime = 0f;
-        private float clickInterval = 0.5f;
-
+        float lastClickTime = 0f;
+        float clickInterval = 1f;
+        bool isUpdatingUI = false;
+        Dialogue currentDialogue;
+        Dialogue lastDialogue;
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetMouseButtonDown(0))
+            if (!isUpdatingUI && Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetMouseButtonDown(0))
             {
                 if (Time.time - lastClickTime > clickInterval)
                 {
-                    currentDialogueIndex++;
-                    if (currentDialogueIndex >= dialogues.Count)
-                    {
-                        currentDialogueIndex = 0;
-                    }
-                    UpdateUI();
-
                     lastClickTime = Time.time;
+                    GoNext();
                 }
             }
+        }
+        private void GoNext()
+        {
+            Debug.Log("GoNext");
+            if (dialogues.Count == 0)
+            {
+                dialogueBox.transform.Find("Panel").gameObject.SetActive(false);
+                currentDialogueIndex = 0;
+                return;
+            }
+            currentDialogue = dialogues[currentDialogueIndex];
+            textTitle.text = currentDialogue.title;
+            textContent.text = "";
+            Texture2D texture;
+            Sprite sprite;
+            switch (currentDialogue.portraitLocation)
+            {
+                case PortraitLocation.Left:
+                    if (lastLeftSource == currentDialogue.portraitSource)
+                        break;
+                    texture = Resources.Load<Texture2D>(currentDialogue.portraitSource);
+                    sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+                    leftPortrait.sprite = sprite;
+                    leftPortrait.SetNativeSize();
+                    break;
+                case PortraitLocation.Right:
+                    if (lastRightSource == currentDialogue.portraitSource)
+                        break;
+                    texture = Resources.Load<Texture2D>(currentDialogue.portraitSource);
+                    sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+                    rightPortrait.sprite = sprite;
+                    rightPortrait.SetNativeSize();
+                    break;
+                default:
+                    break;
+            }
+            dialogues.RemoveAt(0);
+            fullText = currentDialogue.content;
+            isUpdatingUI = true; ;
+            isUpdatingUIText = true;
+            isUpdatingUIImage = true;
+            StartCoroutine(UpdatingText());
+            StartCoroutine(UpdatingImage());
+        }
+
+        bool isUpdatingUIText;
+        bool isUpdatingUIImage;
+
+        private bool IsSameSpeaker()
+        {
+            bool isSame = currentDialogue != null && lastDialogue != null && currentDialogue.portraitLocation == lastDialogue.portraitLocation && currentDialogue.portraitSource == lastDialogue.portraitSource;
+            return isSame;
+        }
+
+        //吐字型展示
+        public float updatingTextDelay = 0.1f;
+        string fullText;
+        IEnumerator UpdatingText()
+        {
+            for (int i = 0; i < fullText.Length; i++)
+            {
+                var currentText = fullText.Substring(i, 1);
+                textContent.text += currentText;
+                yield return new WaitForSeconds(updatingTextDelay);
+            }
+            isUpdatingUIText = false;
+            FinishUpdating();
+        }
+
+        private void FinishUpdating()
+        {
+            lastDialogue = currentDialogue;
+            switch (currentDialogue.portraitLocation)
+            {
+                case PortraitLocation.Left:
+                    lastLeftSource = currentDialogue.portraitSource;
+                    break;
+                case PortraitLocation.Right:
+                    lastRightSource = currentDialogue.portraitSource;
+                    break;
+                default:
+                    break;
+            }
+            isUpdatingUI = isUpdatingUIImage || isUpdatingUIText;
+        }
+
+        public Vector3 moveDirection = new Vector3(100, 0, 0);
+        public float moveTime = 1.0f;
+        public float fadeTime = 1.0f;
+        public float fadeTransparent = 0.5f;
+        public float scaleFactorScaleUp = 1.25f; // 初始缩放比例
+        public float scaleFactorScaleDown = 0.8f; // 初始缩放比例
+        IEnumerator UpdatingImage()
+        {
+            Image imageToShow = null;
+            Image imageToFade = null;
+            switch (currentDialogue.portraitLocation)
+            {
+                case PortraitLocation.Left:
+                    imageToShow = leftPortrait;
+                    imageToFade = rightPortrait;
+                    break;
+                case PortraitLocation.Right:
+                    imageToShow = rightPortrait;
+                    imageToFade = leftPortrait;
+                    break;
+                default:
+                    break;
+            }
+            bool isSame = IsSameSpeaker();
+            float elapsedTime = 0.0f;
+            Color startFadeColor = imageToFade.color;
+            Color endFadeColor = isSame ? startFadeColor : new Color(startFadeColor.r, startFadeColor.g, startFadeColor.b, fadeTransparent);
+            Color startShowColor = imageToShow.color;
+            Color endShowColor = isSame ? startShowColor : new Color(startShowColor.r, startShowColor.g, startShowColor.b, 1);
+            Vector3 startShowScale = imageToShow.transform.localScale;
+            Vector3 endShowScale = isSame ? startShowScale : imageToShow.transform.localScale * scaleFactorScaleUp;
+            Vector3 startFadeScale = imageToFade.transform.localScale;
+            Vector3 endFadeScale = isSame ? startFadeScale : imageToFade.transform.localScale * scaleFactorScaleDown;
+            while (elapsedTime < moveTime)
+            {
+                imageToShow.color = Color.Lerp(startShowColor, endShowColor, elapsedTime / fadeTime);
+                imageToShow.transform.localScale = Vector3.Lerp(startShowScale, endShowScale, elapsedTime / moveTime);
+                imageToFade.color = Color.Lerp(startFadeColor, endFadeColor, elapsedTime / fadeTime);
+                imageToFade.transform.localScale = Vector3.Lerp(startFadeScale, endFadeScale, elapsedTime / moveTime);
+
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            isUpdatingUIImage = false;
+            FinishUpdating();
         }
 
         void LoadDialogueData()
         {
-            // 从外部Json文件加载对话数据
-            string json = File.ReadAllText(dialogueSource);
+            string json = Resources.Load<TextAsset>("Dialogues/DialogueTest").text;
             dialogues = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Dialogue>>(json);
-        }
-
-        void UpdateUI()
-        {
-            if (dialogues.Count == 0)
-            {
-                DialogueBox.transform.Find("Panel").gameObject.SetActive(false);
-                //DialogueBox.GetComponent<Canvas>().enabled = false;
-                return;
-            }
-            var dialogue = dialogues[0];
-            textTitle.text = dialogue.title;
-            textContent.text = dialogue.content;
-            dialogues.RemoveAt(0);
         }
     }
 
-    [System.Serializable]
+    public enum PortraitLocation
+    {
+        None,
+        Left,
+        Right,
+    }
+
+    [Serializable]
     public class Dialogue
     {
+        public PortraitLocation portraitLocation;
+        public string portraitSource;
         public string title;
         public string content;
-
-        public Dialogue(string title, string content)
-        {
-            this.title = title;
-            this.content = content;
-        }
     }
 }
