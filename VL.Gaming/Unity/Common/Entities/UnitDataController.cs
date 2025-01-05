aedb16c78eaea37e7f1a46016863632c5640f630
vlong638 <vlong638@163.com>
2025-01-05 09:17:05 +0800

    找到了对应的Unity包 在Unity的项目资源中打开代码 Assembly-CSharp即可找到应用库 => Unity.TextMeshPro.dll

--- VL.Gaming/Unity/Common/Entities/UnitDataController.cs
+++ VL.Gaming/Unity/Common/Entities/UnitDataController.cs
@@ -1,49 +1,49 @@
-﻿//using TMPro;
-//using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
-//namespace VL.Gaming.Unity.Common.Entities
-//{
-//    internal class UnitDataController : MonoBehaviour
-//    {
-//        public UnitData Data;
-//        public GameObject HPTextGO;
+namespace VL.Gaming.Unity.Common.Entities
+{
+    internal class UnitDataController : MonoBehaviour
+    {
+        public UnitData Data;
+        public GameObject HPTextGO;
 
-//        void Start()
-//        {
-//            if (Data != null)
-//            {
-//                Data.OnHPChanged += UpdateHpText;
-//                Data.HP = Data.HP;
-//            }
-//        }
+        void Start()
+        {
+            if (Data != null)
+            {
+                Data.OnHPChanged += UpdateHpText;
+                Data.HP = Data.HP;
+            }
+        }
 
-//        private float timer = 0f;
-//        void Update()
-//        {
-//            float deltaTime = Time.deltaTime;
-//            timer += deltaTime;
-//            if (timer >= 1f)
-//            {
-//                Data.HP++;
-//                timer = 0f;
-//                Debug.Log("当前Data.HP的值为: " + Data.HP);
-//            }
-//        }
+        private float timer = 0f;
+        void Update()
+        {
+            float deltaTime = Time.deltaTime;
+            timer += deltaTime;
+            if (timer >= 1f)
+            {
+                Data.HP++;
+                timer = 0f;
+                Debug.Log("当前Data.HP的值为: " + Data.HP);
+            }
+        }
 
-//        private void UpdateHpText()
-//        {
-//            if (HPTextGO != null)
-//            {
-//                HPTextGO.GetComponent<TextMeshProUGUI>().text = Data.HP.ToString();
-//            }
-//        }
+        private void UpdateHpText()
+        {
+            if (HPTextGO != null)
+            {
+                HPTextGO.GetComponent<TextMeshProUGUI>().text = Data.HP.ToString();
+            }
+        }
 
-//        void OnDestroy()
-//        {
-//            if (Data != null)
-//            {
-//                Data.OnHPChanged -= UpdateHpText;
-//            }
-//        }
-//    }
-//}
+        void OnDestroy()
+        {
+            if (Data != null)
+            {
+                Data.OnHPChanged -= UpdateHpText;
+            }
+        }
+    }
+}
